fe8f99af6b7b69d90d94e6064e7c2b82a4c3f4db
yassine lamouadden <mouden529@gmail.com>
2023-04-11 12:37:05 +0000

    form client front end

--- client/src/components/utils/Wrappers/Wrapper.js
+++ client/src/components/utils/Wrappers/Wrapper.js
@@ -2,7 +2,7 @@ import React, { Children } from "react";
 
 const Wrapper = ({ children }) => {
     return (
-        <div className="content rounded-t-lg md:rounded-none z-20 overflow-y-auto  h-96 tableScroll ">
+        <div className="content rounded-t-lg md:rounded-none  overflow-y-auto  h-96 tableScroll ">
             <div className="">{children}</div>
         </div>
     );
--- client/src/components/utils/form/FormClient.js
+++ client/src/components/utils/form/FormClient.js
@@ -1,15 +1,50 @@
 import React from "react";
 import LoginInput from "./LoginInput";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { hide } from "../../../store/overlaySlice";
 
 const FormClient = () => {
+    const dispatch = useDispatch();
+    const visibility = useSelector((state) => state.overlay.show);
     return (
-        <div className="bg-white absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-40">
-            <h1>Add client</h1>
-            <form>
+        <div className="bg-white p-6 w-full md:max-w-3xl rounded-lg shadow-lg absolute top-1/2 left-1/2 -translate-y-2/3 -translate-x-1/2 z-40">
+            <div className="flex items-center justify-between mb-10">
+                <h1 className="text-2xl text-blue-500">Ajouter un client</h1>
+                <AiFillCloseCircle
+                    onClick={() => dispatch(hide())}
+                    className="cursor-pointer hover:opacity-75 duration-150 text-red-500"
+                    size={30}
+                />
+            </div>
+
+            <form className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                <LoginInput type="text" placeholder="First Name" />
+                <LoginInput type="text" placeholder="Last Name" />
+
+                <label for="underline_select" class="sr-only">
+                    Underline select
+                </label>
+                <select
+                    id="underline_select"
+                    class="block  pt-6 px-0 w-full text-sm text-gray-400 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                >
+                    <option selected>Personal business</option>
+                    <option value="US">Buyer</option>
+                    <option value="CA">Seller</option>
+                    <option value="FR">Renter</option>
+                    <option value="DE">Big company</option>
+                </select>
+                <LoginInput type="tel" placeholder="Phone" />
+                <LoginInput type="text" placeholder="Address" />
                 <LoginInput type="email" placeholder="Email" />
-                <LoginInput type="email" placeholder="Email" />
-                <LoginInput type="email" placeholder="Email" />
-                <LoginInput type="email" placeholder="Email" />
+
+                <button
+                    type="submit"
+                    class="btn btn-primary mt-8 py-3 px-4 w-full xl:w-32 xl:mr-3 align-top"
+                >
+                    Ajouter
+                </button>
             </form>
         </div>
     );
